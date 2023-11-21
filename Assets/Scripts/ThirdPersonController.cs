40d8d385674273bbad420bcef1ea42f634005346
Dmitry OLeD <dm.bashkirov@gmail.com>
2023-11-21 21:51:02 +0300

    1_1
    Dash system

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -21,6 +21,14 @@ public class ThirdPersonController : MonoBehaviour
     private Vector3 forceDirection = Vector3.zero;
 
 
+    //dash
+    public float dashDistance = 12f;
+    public float dashTime = 0.2f;
+    private bool isDashing = false;
+    private float dashTimer = 0f;
+
+
+    //camera
     [SerializeField]
     private Camera playerCamera;
 
@@ -61,8 +69,31 @@ public class ThirdPersonController : MonoBehaviour
 
         LookAt();
 
+        
     }
 
+
+    private void Update()
+    {
+        // Check for input and initiate dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+
+        // Update dash timer
+        if (isDashing)
+        {
+            dashTimer += Time.deltaTime;
+
+            // Check if dash duration is over
+            if (dashTimer >= dashTime)
+            {
+                isDashing = false;
+                dashTimer = 0f;
+            }
+        }
+    }
 
     private void LookAt()
     {
@@ -107,4 +138,24 @@ public class ThirdPersonController : MonoBehaviour
         else
             return false;
     }
+
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+
+        // Store initial position
+        Vector3 startPosition = transform.position;
+
+        // Calculate dash end position
+        Vector3 endPosition = startPosition + transform.forward * dashDistance;
+
+        // Perform the dash
+        while (isDashing)
+        {
+            rb.MovePosition(Vector3.Lerp(startPosition, endPosition, dashTimer / dashTime));
+            yield return null;
+        }
+    }
+
 }
