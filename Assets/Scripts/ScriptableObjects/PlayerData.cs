9f8d3d5565f8d1f2197dd4d1a42fa4ae62ce0520
Dimapsv <dm.bashkirov@gmail.com>
2025-01-17 13:42:23 +0300

    10

---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public bool isDashingUpgrade;
+    public bool isPushingUpgrade;
+    public bool isRadarUpgrade;
+
+
+}
